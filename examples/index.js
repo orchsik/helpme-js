79768765f4c0273a2465e60517c05698dccade89
orchsik <orchsik@naver.com>
2021-12-18 22:23:56 +0900

    resurrect: prepublishOnly

--- examples/index.js
+++ examples/index.js
@@ -1,1 +1,4 @@
-const { HColors } = require("helpme-js");
+const { HColors, HMath } = require('helpme-js');
+const { ESTIMATE_TYPE } = require('helpme-js/dist/math');
+
+HMath.estimateNumber(12.34, 2, ESTIMATE_TYPE.CEIL);
